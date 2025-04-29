543e682f8822ca2f3a6557d0bf21e96fb53fe4fa
Enzo Canavero <ecanavero2@gmail.com>
2025-04-29 09:57:27 -0300

    vai gabriel testa ai

--- notes-frontend/src/main.jsx
+++ notes-frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Ministerio, {
+  loader as ministerioLoader,
+} from "./components/Ministerio/index.jsx"; // <-- importe o loader
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Ministerio />,
+    loader: ministerioLoader, // <-- adicione aqui
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
