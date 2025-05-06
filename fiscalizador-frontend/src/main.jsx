e88e12dd594707735edeb81c7458ed67f3c0e8e0
Gabriel <gabrielac7@al.insper.edu>
2025-05-06 08:10:48 -0300

    commit

---
+++ fiscalizador-frontend/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
+import Overview from './components/Overview/Overview.jsx'
+import Noticias from './components/Noticias/Noticias.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "overview/:SIAFE",
+        element: <Overview />
+      },
+      {
+        path: "noticias",
+        element: <Noticias />
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
--- notes-frontend/src/components/AppBar/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-
-export default function AppBar() {
-    return (
-        <div className="appbar">
-            <span className="subtitle">Fiscalizador</span>
-        </div>
-    );
-}
